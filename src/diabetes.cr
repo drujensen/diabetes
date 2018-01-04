5f24f77ad07132758b9a9dff2c1795ea42141214
Dru Jensen <drujensen@gmail.com>
2018-01-03 16:03:24 -0800

    add accuracy

--- src/diabetes.cr
+++ src/diabetes.cr
@@ -4,7 +4,7 @@ require "shainet"
 # use binary dummy columns for `Outcome` column
 label = {
   "0" => [1.to_f64, 0.to_f64],
-  "1"  => [0.to_f64, 1.to_f64],
+  "1" => [0.to_f64, 1.to_f64],
 }
 
 # data structures to hold the input and results
@@ -16,10 +16,10 @@ raw = File.read("./data/diabetes.csv")
 csv = CSV.new(raw, headers: true)
 
 # we don't want these columns so we won't load them
-headers = csv.headers.reject {|h| ["SkinThickness", "DiabetesPedigreeFunction", "Outcome"].includes?(h)}
+headers = csv.headers.reject { |h| ["SkinThickness", "DiabetesPedigreeFunction", "Outcome"].includes?(h) }
 
 # load the data structures
-while(csv.next)
+while (csv.next)
   row_arr = Array(Float64).new
   headers.each do |header|
     row_arr << csv.row[header].to_f64
@@ -34,14 +34,44 @@ normalized.normalize_min_max
 
 # create a network
 diabetes : SHAInet::Network = SHAInet::Network.new
-diabetes.add_layer(:input, 6, :memory)
-diabetes.add_layer(:hidden, 8, :memory)
-diabetes.add_layer(:output, 2, :memory)
+diabetes.add_layer(:input, 6, "memory", SHAInet.relu)
+diabetes.add_layer(:hidden, 8, "memory", SHAInet.relu)
+diabetes.add_layer(:output, 2, "memory", SHAInet.sigmoid)
 diabetes.fully_connect
+diabetes.learning_rate = 0.01
+diabetes.momentum = 0.01
 
 # train the network
-diabetes.train_batch(normalized.data, :adam, :mse, :sigmoid, 50000, 0.01)
+# diabetes.train_batch(normalized.data.shuffle, :adam, :mse, epoch = 1000, threshold = 0.00001, log = 1000, batches = 50)
+diabetes.train(normalized.data.shuffle, :sgdm, :mse, epoch = 30000, threshold = 0.00000001, log = 100)
 
-# try it out and verify it works
-diabetes.run(normalized.normalized_inputs.first)
-diabetes.run(normalized.normalized_inputs.last)
+# save to file
+diabetes.save_to_file("./model/diabetes.nn")
+
+tn = tp = fn = fp = 0
+
+# determine accuracy
+normalized.normalized_inputs.each_with_index do |test, idx|
+  results = diabetes.run(test)
+  if results[0] < 0.5
+    if outputs[idx][0] == 0.0
+      tn += 1
+    else
+      fn += 1
+    end
+  else
+    if outputs[idx][0] == 0.0
+      fp += 1
+    else
+      tp += 1
+    end
+  end
+end
+
+puts "Training size: #{outputs.size}"
+puts "----------------------"
+puts "TN: #{tn} | FP: #{fp}"
+puts "----------------------"
+puts "FN: #{fn} | TP: #{tp}"
+puts "----------------------"
+puts "Accuracy: #{(tn + tp) / outputs.size.to_f}"
